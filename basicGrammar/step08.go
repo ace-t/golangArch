c6e86464c53c61582815cbf03ca2c48c1cb239f0
acetaeha <acetaeha@hanmail.net>
2018-03-12 16:32:13 +0900

    step08 use flag

--- basicGrammar/step08.go
+++ basicGrammar/step08.go
@@ -1,10 +1,15 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 /*
 
 간단한 switch문
 바로 코딩하려는데 잠시 생각하게 만들어서 작성 해봄.
-go는 여러개의 조건문일 때 if 보다는 switch문을 선호한다. 
+go는 여러개의 조건문일 때 if 보다는 switch문을 선호한다.
 
  */
 func main() {
@@ -24,4 +29,21 @@ func main() {
 	}
 
 	println(name )
+
+	flags()
+
+}
+
+var (
+	configPath string
+)
+
+func flags(){
+	var ip = flag.Int("ip", 1234, "help message for flagname")
+	flag.StringVar(&configPath, "config","", "config file")
+	flag.Parse()
+	fmt.Println(*ip)
+	fmt.Println("flag.Arg(int):", flag.Arg(0))
+	fmt.Println("flag.Args():", flag.Args())
+	fmt.Println(configPath)
 }
