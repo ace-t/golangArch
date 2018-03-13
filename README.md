c081685006d0a6a2892b93aeddbe8cc9c51a72bc
acetaeha <acetaeha@hanmail.net>
2018-03-13 15:50:29 +0900

    step10 goroutine + closure + for range 관련 소스

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # golangArch
 * basic grammar를 통해 golang 철학 배우기
 * http://acet.pe.kr/category/Language/go%20lang 등 가이드 문서 작성
+  * step01 : about Struct 기본
+  * step02 : about receiver
+  * step03 : about Interface
+  * step04 : about http
+  * step05 : about json
+  * step06 : about chi
+  * step07 : about 다중 struct
+  * step08 : about flag
+  * step09 : about toml(config)
+  * step10 : about goroutine
+  * step11 : about channel
+  * step12 : about panic&recover
---
+++ basicGrammar/step10.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func goroutineFunc(name string){
+	for i :=0; i < 3; i++ {
+		fmt.Println(name, "****", i)
+	}
+}
+
+func goroutineRanageFor(){
+	// 함수 밖 변수 선언
+	names := []string{"에이스티","애니자바","굿택","틴톨"}
+	for idx, name := range names {
+		name := name  // 이 소스에서 가장 핵심적인 부분 이다. range안의 범위에서는 namme이 모두 같은 주소로 나오게 된다.
+		println(&name, name)
+		go func(){  // goroutine 익명함수
+			println(name,idx)
+			goroutineFunc(name)
+		}()
+	}
+
+	//fmt.Scanln()
+	time.Sleep(time.Second*3)
+
+	/*
+	result : 같은 주소에 담겨진다.
+	0xc420043f00   에이스티
+    0xc420043f00   애니자바
+    0xc420043f00   굿택
+    0xc420043f00   틴톨
+
+	name := name 선언 후에는 주소값이 다르다.
+	0xc42000e1e0 에이스티
+	0xc42000e1f0 애니자바
+	0xc42000e200 굿택
+	0xc42000e210 틴톨
+
+	 */
+}
+
+// go 루틴에 대해서! 알아보자~
+// go는 디폴트로 하나의 CPU를 사용. 여러개의 CPU 사용한다면 다중 CPU에서 병렬처리를할 수 있도록 아래와 같은 함수를 호출하여야 한다.
+// runtime.GOMAXPROCS(cpu수)함수를 호출.
+func main() {
+	// 함수를 동기적으로 수행
+	//goroutineFunc("태하-Sync")
+
+	// 함수를 비동기적으로 수행.
+	//go goroutineFunc("미애-Async")
+	//go goroutineFunc("한전-Async")
+	//go goroutineFunc("영금-Async")
+	goroutineRanageFor()
+
+
+
+}
+
+
