040e769f20843a9ad59da15a4768642f84c11c4e
acetaeha <acetaeha@hanmail.net>
2018-03-13 16:06:46 +0900

    일반적인 for 문에서도 테스트(range와 동일한 결과)

--- basicGrammar/step10.go
+++ basicGrammar/step10.go
@@ -42,6 +42,33 @@ func goroutineRanageFor(){
 	 */
 }
 
+
+func goroutineGenFor(){
+	// 함수 밖 변수 선언
+	names := []string{"에이스티","애니자바","굿택","틴톨"}
+	for i:=0; i < len(names); i++{
+		i := i  // ranage나 일반적인 for나 동일하다.
+		go func(){  // goroutine 익명함수
+			println(i,names[0])
+			println(i,names[1])
+		}()
+	}
+	time.Sleep(time.Second*3)
+
+	/*
+	result
+		1 에이스티
+		1 애니자바
+		2 에이스티
+		2 애니자바
+		3 에이스티
+		3 애니자바
+		0 에이스티
+		0 애니자바
+	 */
+
+}
+
 // go 루틴에 대해서! 알아보자~
 // go는 디폴트로 하나의 CPU를 사용. 여러개의 CPU 사용한다면 다중 CPU에서 병렬처리를할 수 있도록 아래와 같은 함수를 호출하여야 한다.
 // runtime.GOMAXPROCS(cpu수)함수를 호출.
@@ -53,7 +80,9 @@ func main() {
 	//go goroutineFunc("미애-Async")
 	//go goroutineFunc("한전-Async")
 	//go goroutineFunc("영금-Async")
-	goroutineRanageFor()
+
+	//goroutineRanageFor()
+	goroutineGenFor()
 
 
 
